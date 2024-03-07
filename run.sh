6dd8f956418af0dfc2a10151b0b8506f5f1e6732
Ibrahim Damlaj <idamlaj@gmail.com>
2024-03-07 14:37:50 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 # run this script with the following command:
 # torchrun --no-python --nnodes 1 --nproc-per-node 2 ./run.sh
 
+# Having run.sh file like this is preferred to running hello.py directly,
+# because it allows us to add setup / teardown commands before the main python
+# code is executed.
+
 # gets the directory containing this script, cd, then runs hello.py
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR &&
